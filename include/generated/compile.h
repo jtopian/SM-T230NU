344754b3833daa9c78c7a55e8349b0769f553d06
JT Anderson <xaxas@trilobyte.jtopia.org>
2020-07-08 17:11:00 -0700

    Stock kernel source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 30 19:32:34 KST 2016"
+#define LINUX_COMPILE_BY "kihyuk"
+#define LINUX_COMPILE_HOST "kihyuk-Samsung-DeskTop-System"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
